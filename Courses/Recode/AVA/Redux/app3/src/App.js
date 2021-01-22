49f8559f1f515ce050fd536dc95dfdd87187a689
Noslig <gilszon@gmail.com>
2021-01-21 23:38:37 -0300

    Redux from scratch

---
+++ Courses/Recode/AVA/Redux/app3/src/App.js
@@ -0,0 +1,18 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App" style={{ paddingTop: '10px' }}>
+        <h2>App3: React com Redux</h2>
+          <input type='text' />
+              <button>
+                  Enviar
+              </button>
+          <h3>Aguardando...</h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
