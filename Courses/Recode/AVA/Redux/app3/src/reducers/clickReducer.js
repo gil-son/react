742527b13fef343a5fdf47f91b7d9add0cf4a3fc
Noslig <gilszon@gmail.com>
2021-01-22 17:10:33 -0300

    Reducers

---
+++ Courses/Recode/AVA/Redux/app3/src/reducers/clickReducer.js
@@ -0,0 +1,20 @@
+import {CLICK_UPDATE_VALUE} from '../action'
+
+
+const initialState = {
+    newValeu:''
+};
+
+export const clickReducer = (state = initialState, action) =>{
+
+    switch (action.type) {
+        case CLICK_UPDATE_VALUE:
+        return{
+            ...state,
+            newValue: action.newValue
+        }
+        default:
+            return state;
+    }
+
+};
